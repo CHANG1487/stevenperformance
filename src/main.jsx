9864949434667d40cc82fe9dbce814d075110ccd
Lika <heler422825@gmail.com>
2026-06-24 10:37:46 +0800

    部署

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
-      <BrowserRouter>
+      <BrowserRouter basename="/stevenperformance">
         <AuthProvider>
           <App />
         </AuthProvider>
